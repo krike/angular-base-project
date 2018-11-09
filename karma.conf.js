9edb4a8f2721872a50acfc1d726b76aa0adf06f4
Christophe Debruel <krike06@gmail.com>
2018-11-09 22:14:00 +0100

    upgrade packages and upgrade to Angular 7

--- karma.conf.js
+++ karma.conf.js
@@ -4,21 +4,19 @@
 module.exports = function (config) {
     config.set({
         basePath: '',
-        frameworks: ['jasmine', '@angular/cli'],
+        frameworks: ['jasmine', '@angular-devkit/build-angular'],
         plugins: [
             require('karma-jasmine'),
             require('karma-chai'),
             require('karma-chrome-launcher'),
             require('karma-nyan-reporter'),
             require('karma-clear-terminal-reporter'),
-            require('@angular/cli/plugins/karma')
+            require('@angular-devkit/build-angular/plugins/karma')
         ],
         client: {
             clearContext: false // leave Jasmine Spec Runner output visible in browser
         },
-        angularCli: {
-            environment: 'dev'
-        },
+        
         reporters: ['nyan', 'clear-terminal'],
         port: 9876,
         colors: true,
